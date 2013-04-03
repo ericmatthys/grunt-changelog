71df9f499f7d130050b1097a8bd34b0e9887c66c
Eric Matthys <ericmatthys@gmail.com>
2013-04-02 23:13:36 -0600

    closes #452: sample message.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,57 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>',
+			],
+			options: {
+				jshintrc: '.jshintrc',
+			},
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp'],
+		},
 
-    // Configuration to be run (and then tested).
-    changelog: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
+		// Configuration to be run (and then tested).
+		changelog: {
+			default_options: {},
+			custom_options: {
+				options: {
+					before: '2013-04-01',
+					after: '2013-03-26',
+					dest: 'tmp/changelog.txt'
+				}
+			},
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js'],
+		}
+	});
 
-  });
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'changelog', 'nodeunit']);
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'changelog', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/changelog.js
+++ tasks/changelog.js
@@ -10,16 +10,56 @@
 
 module.exports = function (grunt) {
 
+	var moment = require('moment');
+
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
 
 	grunt.registerMultiTask('changelog', 'Creates a list of changes based on a git log.', function () {
 		// Merge task-specific and/or target-specific options with these defaults.
 		var options = this.options({
+			after: moment().subtract('days', 7).format(),
+			before: moment().format(),
+			featureRegex: /([^\n]*)closes #\d+([^\n]+)*/gi,
+			bugRegex: /([^\n]*)closes #\d+([^\n]+)*/gi,
 			dest: 'changelog.txt'
 		});
 
-		grunt.verbose.writeflags(options, 'Options');
+		grunt.log.writeflags(options, 'Options');
+
+		var done = this.async();
+		var args = ['log', '--pretty=format:%s', '--no-merges'];
+
+		args.push('--after="' + options.after + '"');
+		args.push('--before="' + options.before + '"');
+
+		grunt.log.writeln('git ' + args.join(' ') + '\n' );
+
+		grunt.util.spawn(
+			{
+				cmd: 'git',
+				args: args
+			},
+
+			function (error, result) {
+				if (error) {
+					grunt.log.error(error);
+					return done(false);
+				}
+
+				var match;
+
+				while ((match = options.featureRegex.exec(result))) {
+					grunt.log.ok('match');
+					grunt.log.writeflags(match);
+				}
+
+				grunt.file.write(options.dest, result);
+				grunt.log.ok('Changelog written to '+ options.dest);
+
+				done();
+			}
+		);
 	});
 
 };
